184a0a89440c88e2780f69a7737e136ae6392bca
Joao Felipe Vilas Boas <127103088+Vilasz@users.noreply.github.com>
2026-04-05 23:31:51 -0300

    Fix: alb module variables

--- infra/terraform/modules/alb/variables.tf
+++ infra/terraform/modules/alb/variables.tf
@@ -1,91 +1,19 @@
 variable "project_name" {
-  type = string
-}
-
-variable "environment" {
-  type = string
+  description = "Project name prefix"
+  type        = string
 }
 
 variable "vpc_id" {
-  type = string
+  description = "VPC ID where the ALB and target groups will be created"
+  type        = string
 }
 
 variable "public_subnet_ids" {
-  type = list(string)
+  description = "Public subnet IDs for the ALB"
+  type        = list(string)
 }
 
-variable "security_group_id" {
-  type = string
-}
-
-variable "tracking_service_image" {
-  description = "ECR repository URL for tracking-service"
+variable "alb_security_group_id" {
+  description = "Security group ID attached to the ALB"
   type        = string
-}
-
-variable "order_service_image" {
-  description = "ECR repository URL for order-service"
-  type        = string
-}
-
-variable "tracking_target_group_arn" {
-  type = string
-}
-
-variable "order_target_group_arn" {
-  type = string
-}
-
-variable "alb_dns_name" {
-  type = string
-}
-
-variable "tracking_cpu" {
-  type    = number
-  default = 512
-}
-
-variable "tracking_memory" {
-  type    = number
-  default = 1024
-}
-
-variable "tracking_desired" {
-  type    = number
-  default = 1
-}
-
-variable "tracking_min" {
-  type    = number
-  default = 1
-}
-
-variable "tracking_max" {
-  type    = number
-  default = 2
-}
-
-variable "order_cpu" {
-  type    = number
-  default = 512
-}
-
-variable "order_memory" {
-  type    = number
-  default = 1024
-}
-
-variable "order_desired" {
-  type    = number
-  default = 1
-}
-
-variable "order_min" {
-  type    = number
-  default = 1
-}
-
-variable "order_max" {
-  type    = number
-  default = 2
 }
