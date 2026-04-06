b221198c363bea1f1782d1fbf067e50edbc61d95
Joao Felipe Vilas Boas <127103088+Vilasz@users.noreply.github.com>
2026-04-05 23:34:12 -0300

    Small fix

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -128,11 +128,10 @@ module "dynamodb" {
 module "alb" {
   source = "./modules/alb"
 
-  project_name      = var.project_name
-  environment       = var.environment
-  vpc_id            = module.networking.vpc_id
-  public_subnet_ids = module.networking.public_subnet_ids
-  security_group_id = aws_security_group.alb.id
+  project_name          = var.project_name
+  vpc_id                = module.networking.vpc_id
+  public_subnet_ids     = module.networking.public_subnet_ids
+  alb_security_group_id = aws_security_group.alb.id
 }
 
 # ──────────────────────────────────────────────
